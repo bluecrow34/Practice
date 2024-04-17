96656e68b75ef4c8ccc5bb7dcee8fc069d340a62
Cam DeSousa <134101013+bluecrow34@users.noreply.github.com>
2024-04-17 14:46:39 -0400

    Create app.js

---
+++ 40.10/src/app.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Deck from "./Deck"
+
+function App() {
+  return (
+    <div className="App">
+      <Deck/>
+    </div>
+  );
+}
+
+export default App;
